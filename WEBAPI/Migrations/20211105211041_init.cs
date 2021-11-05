d2b78a11fd1c5d1730037219510b31413d8279df
unknown <faridun.nk@gmail.com>
2021-11-06 03:44:08 +0500

    WebApi

---
+++ WEBAPI/Migrations/20211105211041_init.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WEBAPI.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Quotes",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "InsertDate", "Text" },
+                values: new object[] { new DateTime(2021, 11, 6, 2, 10, 40, 541, DateTimeKind.Local).AddTicks(40), "Если ты устал, то присядь" });
+
+            migrationBuilder.UpdateData(
+                table: "Quotes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "InsertDate",
+                value: new DateTime(2021, 11, 6, 2, 10, 40, 543, DateTimeKind.Local).AddTicks(41));
+
+            migrationBuilder.UpdateData(
+                table: "Quotes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "InsertDate",
+                value: new DateTime(2021, 11, 6, 2, 10, 40, 543, DateTimeKind.Local).AddTicks(41));
+
+            migrationBuilder.UpdateData(
+                table: "Quotes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "InsertDate",
+                value: new DateTime(2021, 11, 6, 2, 10, 40, 543, DateTimeKind.Local).AddTicks(41));
+
+            migrationBuilder.UpdateData(
+                table: "Quotes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "InsertDate",
+                value: new DateTime(2021, 11, 6, 2, 10, 40, 543, DateTimeKind.Local).AddTicks(41));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Quotes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Text",
+                value: "Если ты устал, то присять");
+        }
+    }
+}
---
+++ WEBAPI/Models/Quote.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBAPI.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Текст обязателен")]
+        public string Text { get; set; }
+
+        public string Author { get; set; }
+
+        [Required(ErrorMessage ="Дата должна присуствовать")]
+        public DateTime InsertDate { get; set; }
+
+    }
+}
